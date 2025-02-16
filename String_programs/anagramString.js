51384f5018bd2e5f1cc4c8815241db26b9dfeaf0
lalitkush1991 <lalitkush1991@gmail.com>
2025-02-16 14:18:04 +0530

    new programs

---
+++ String_programs/anagramString.js
@@ -0,0 +1,17 @@
+
+function checkAnagrams(str1,str2){
+    let str1Sorted = str1.split('').sort().join('');
+    let str2Sorted = str2.split('').sort().join('');
+
+    if(str1Sorted === str2Sorted){
+        console.log("The strings are anagrams");
+    }
+    else {
+        console.log("The strings are not anagrams");
+    }
+
+}
+
+let str1 = "listen";
+let str2 = "silent";
+checkAnagrams(str1,str2);
---
+++ String_programs/removeDuplicatesFromArray.js
@@ -0,0 +1,22 @@
+let numArray = [1,2,3,4,5,6,7,3,4,5];
+
+function removeDuplicates(numArray) {
+    let uniqueArray = [];
+    for (let i = 0; i < numArray.length; i++) {
+        if (uniqueArray.indexOf(numArray[i]) == -1) {
+            uniqueArray.push(numArray[i]);
+        }
+    }
+    return uniqueArray;
+}
+
+console.log(removeDuplicates(numArray)); 
+
+
+function removeDuplicatesUsingSet(numArray) {
+    let uniqueArray1 = [...new Set(numArray)];
+    return uniqueArray1;
+}
+
+console.log(removeDuplicatesUsingSet(numArray)); 
+
