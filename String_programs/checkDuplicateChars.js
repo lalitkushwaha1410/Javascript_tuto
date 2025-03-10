421773373dd25350f39aa4651b43067a33e42831
lalitkush1991 <lalitkush1991@gmail.com>
2025-03-11 00:14:32 +0530

    new programs

--- String_programs/checkDuplicateChars.js
+++ String_programs/checkDuplicateChars.js
@@ -1,6 +1,13 @@
-let str1 = "occurrence";
+// print duplicate characters in an array
+//let str1 = "occurence";
+let str1 = [4,6,3,6,9,2,4,7,5,8,9,5];
 let duplicates = [...new Set([...str1].filter((value, index, str1) => 
   str1.indexOf(value) !== index )
 )];
 
 console.log(duplicates);
+
+
+let result = [new Set([...str1].filter((value, index, str1) =>
+  str1.indexOf(value) !== index) 
+)];
