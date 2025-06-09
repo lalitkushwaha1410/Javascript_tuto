977160265c1edb12bb42b2af00ac56022a03d078
Lalit Kushwaha <Lalit.Kushwaha@lexisnexisrisk.com>
2025-06-09 15:57:01 +0530

    bubble sort added

---
+++ String_programs/bubbleSort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// Example
+let numbers = [49,2,67,43,78,8,12,19];
+console.log("Sorted array:", bubbleSort(numbers));
