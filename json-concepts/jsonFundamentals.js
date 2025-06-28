56ab2d8e85fea5a38bdc4c590d0601e22034e2f2
Lalit Kushwaha <Lalit.Kushwaha@lexisnexisrisk.com>
2025-06-28 14:31:38 +0530

    json operations

--- json-concepts/jsonFundamentals.js
+++ json-concepts/jsonFundamentals.js
@@ -1,17 +1,75 @@
 let jsonObj = {
-    name : "Rohan",
-    channel : "Discovery",
-    friend : "David",
-    Food : "Pizza"
+    "users" : [
+    { 
+        "name" : "Rohit",
+        "channel" : "Discovery",
+        "friend" : "David",
+        "Food" : "Pizza",
+        "skills" : ["html" , "java", "python"],
+        "address" : {
+            "city" :"Pune",
+            "State" : "Maharashtra",
+            "Country" : "India"
+        } 
+    },
+    {
+         "name" : "Virat",
+        "channel" : "Zee cinema",
+        "friend" : "David",
+        "Food" : "Momoz",
+        "skills" : ["html" , "java", "python"],
+        "address" : {
+            "city" :"Pune",
+            "State" : "Maharashtra",
+            "Country" : "India"
+        } 
+    },
+    { "name" : "Abhimanyu",
+        "channel" : "AAJtak",
+        "friend" : "David",
+        "Food" : "Fruits",
+        "skills" : ["html" , "java", "python"],
+        "address" : {
+            "city" :"Agra",
+            "State" : "Uttar Pradesh",
+            "Country" : "India"
+        } 
+    } , 
+        
+    { "name" : "Saurabh",
+        "channel" : "Sony",
+        "friend" : "David",
+        "Food" : "Coke",
+        "skills" : ["html" , "java", "python"],
+        "address" : {
+            "city" :"Ahmedabad",
+            "State" : "Gujarat",
+            "Country" : "India"
+        } 
+    } 
+]
+    
+    }
 
+
+// console.log(jsonObj);
+// console.log(jsonObj.Food);
+// console.log(jsonObj.skills[0]);
+// console.log(jsonObj.address.State);
+
+// jsonObj.users.forEach(user => {    // print all users name 
+//   console.log(user.name);
+// });
+
+for (let i = 0; i < jsonObj.users.length; i++) {
+  console.log(jsonObj.users[i].name);
 }
-console.log(jsonObj);
 
-let myJsonStr = JSON.stringify(jsonObj); // convert to string type
-console.log(myJsonStr);
+// let myJsonStr = JSON.stringify(jsonObj); // convert to string type to send over network to server
+// console.log(myJsonStr);
 
-myJsonStr = myJsonStr.replace("Rohan" , "Virat"); // replace values in string
-console.log(myJsonStr);
+// myJsonStr = myJsonStr.replace("Rohan" , "Virat"); // replace values in string
+// console.log(myJsonStr);
 
-newJsonObj = JSON.parse(myJsonStr);
-console.log(newJsonObj);
+// newJsonObj = JSON.parse(myJsonStr);
+// console.log(newJsonObj);
