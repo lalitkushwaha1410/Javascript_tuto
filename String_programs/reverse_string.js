95e98667153b7b143b53e176a3dec8091e15a2b5
Lalit Kushwaha <Lalit.Kushwaha@lexisnexisrisk.com>
2025-07-07 01:41:30 +0530

    new changes

--- String_programs/reverse_string.js
+++ String_programs/reverse_string.js
@@ -10,3 +10,30 @@ for (let i=OriginalString.length-1; i>=0; i--){
 }
 
 console.log(result);
+
+
+console.log('----------------------------------------------------------------------------------------------------------');
+
+function reverseEachWord(sentence) {
+  const words = sentence.split(' ');
+  const reversedWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let reversedWord = '';
+
+    for (let j = word.length - 1; j >= 0; j--) {
+      reversedWord += word[j];
+    }
+
+    reversedWords.push(reversedWord);
+  }
+
+  return reversedWords.join(' ');
+}
+
+// Example usage
+const input = "Hello World from ChatGPT";
+const output = reverseEachWord(input);
+console.log(output); // "olleH dlroW morf TPGTahC"
+
