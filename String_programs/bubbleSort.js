7ea15dce23777e509228726eda72512a97fdc05b
Lalit Kushwaha <Lalit.Kushwaha@lexisnexisrisk.com>
2025-07-09 00:19:13 +0530

    www

--- String_programs/bubbleSort.js
+++ String_programs/bubbleSort.js
@@ -13,5 +13,20 @@ function bubbleSort(arr) {
 }
 
 // Example
-let numbers = [49,2,67,43,78,8,12,19];
+let numbers = [49,2,8,12,19];
 console.log("Sorted array:", bubbleSort(numbers));
+
+function sortStringsBasic(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sortStringsBasic(["cat", "apple", "banana"])); // ["apple", "banana", "cat"]
