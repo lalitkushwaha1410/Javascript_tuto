4ffe47d70f3c275daab5583d2c538392cb7acf35
Lalit Kushwaha <Lalit.Kushwaha@lexisnexisrisk.com>
2025-07-14 01:54:09 +0530

    new changes

--- String_programs/rough.js
+++ String_programs/rough.js
@@ -22,3 +22,39 @@ function capitalizeWords(str) {
 let sentence = "hello world this is javascript";
 let capitalized = capitalizeWords(sentence);
 console.log(capitalized); 
+
+
+console.log("---------------------------------------------------------------------------");
+
+function reverseEachWord(sentence) {
+  let words = sentence.split(" ");
+  let result = "";
+  for (let word of words) {
+    result += word.split("").reverse().join("") + " ";
+  }
+  return result.trim();
+}
+
+console.log(reverseEachWord("hello world"));
+
+
+console.log("**********************************************************************************");
+const mystr = "run123test456";
+const matches = mystr.match(/\d/g); // \d+ matches one or more digits
+const myresult = matches.join("");   // Join array into a single string
+console.log(myresult); // Output: "123456"
+
+console.log("##################################################################################");
+
+let testStr = "i want to learn javascript";
+
+let words = testStr.split(" ");
+
+let result2 = "";
+for (let word of words) 
+{
+      result2 = result2 + word.charAt(0).toUpperCase() + word.slice(1) + " ";
+}
+
+
+console.log(result2);
