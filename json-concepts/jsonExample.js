04365f14ca30db9430732ee33c8952a0ac021110
Lalit Kushwaha <lalitkushwaha1410@gmail.com>
2026-06-22 00:59:41 +0530

    333

--- json-concepts/jsonExample.js
+++ json-concepts/jsonExample.js
@@ -4,22 +4,25 @@ let myJson = {
         "name" : "David",
         "org":"microsoft",
         "place":"US",
-        "pincode": 201234,        
+        "pincode": 201234      
     },
     {
         "name" : "Tiger",
         "org":"google",
         "place":"japan",
-        "pincode": 123567,
+        "pincode": 123567
     },{
 
         "name" : "mark",
         "org":"hyndai",
         "place":"korea",
-        "pincode": 431234,
+        "pincode": 431234
     }
 ]
 }
 myJson.persons.forEach( person => console.log(person.name));
 
 myJson.persons.forEach( person => console.log(person.pincode));
+
+let myString = JSON.stringify(myJson);
+console.log(myString);
