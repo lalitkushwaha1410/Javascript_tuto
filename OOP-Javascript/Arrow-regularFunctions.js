ec65c2db4a8b2df6bd9080322d1090525caa3e60
Lalit Kushwaha <lalitkushwaha1410@gmail.com>
2026-06-22 10:36:08 +0530

    eee

--- OOP-Javascript/Arrow-regularFunctions.js
+++ OOP-Javascript/Arrow-regularFunctions.js
@@ -9,14 +9,14 @@
 
 // global scope , object scope 
 
-function myRegularFunction(){ // regular function refers to user1 object
+function myRegularFunction() { // regular function refers to user1 object
     console.log(`Hello ${this.name} - ${this.age}`);
 }
 
 var firstname = 'Javascript'; //global scope object
 var currentage = 78;
 const myArrowFunction = () => {  //arrow function refers to global object
-  
+
     console.log(`Hello ${this.firstname} - ${this.currentage}`);
 }
 
@@ -24,8 +24,8 @@ const user1 = {
     name: 'Ankur',
     age: 30,
     myReg: myRegularFunction, // print Ankur - 30 because regular function has its own this value
-    myArrow : myArrowFunction // print undefined - undefined because arrow function does not have its own this , refers to global object
+    myArrow: myArrowFunction // print undefined - undefined because arrow function does not have its own this , refers to global object
 };
 
-user1.myReg(); 
+user1.myReg();
 user1.myArrow();
