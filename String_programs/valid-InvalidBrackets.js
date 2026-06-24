f5ed200369af3407404f3bb2bdd3bb45225bb8d8
Lalit Kushwaha <lalitkushwaha1410@gmail.com>
2026-06-24 13:27:10 +0530

    ww

--- String_programs/valid-InvalidBrackets.js
+++ String_programs/valid-InvalidBrackets.js
@@ -3,14 +3,14 @@ function isValidBracketString(str) {
     do {
         prevStr = str;
         str = str.replace("()", "").replace("{}", "").replace("[]", "");
-    } 
+    }
     while (str !== prevStr);
-    
+
     return str.length === 0;
 }
 
 // Test cases
-console.log(isValidBracketString("({})"));     // true
+console.log(isValidBracketString("({})[]"));     // true
 console.log(isValidBracketString("[(]"));      // false
 console.log(isValidBracketString("[]{}()"));   // true
 console.log(isValidBracketString("([)]"));     // false
